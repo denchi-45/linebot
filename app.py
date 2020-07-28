25206615252bde4ee9f9274aa37baaec9bd0542e
Denchi <hori.takoashi45@gmail.com>
2020-07-29 01:19:12 +0900

    new commit

--- app.py
+++ app.py
@@ -40,9 +40,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    
+    if event.messege.text == "必要ない":
+        line_bot_api.reply_message(event.reply_token, TextSendMessage("うっうっ"))
+    
+    else:   
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
