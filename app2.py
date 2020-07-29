c95482d7f1fe2d3915c75c30f1134cceb1e5114d
Denchi <hori.takoashi45@gmail.com>
2020-07-29 18:00:46 +0900

    new commit

--- app2.py
+++ app2.py
@@ -1,5 +1,12 @@
-from flask import Flask, request, abort
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 17:59:17 2020
 
+@author: horit
+"""
+import os
+
+from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -7,29 +14,25 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-import os
+    MessageEvent, TextMessage, MessageAction, TemplateSendMessage,
+    ButtonsTemplate)
 
 app = Flask(__name__)
 
-#環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -41,7 +44,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def response_message(event):
     profile = line_bot_api.get_profile(event.source.user_id)
-    
+
     status_msg = profile.status_message
     if status_msg != "None":
         # LINEに登録されているstatus_messageが空の場合は、"なし"という文字列を代わりの値とする
@@ -54,33 +57,10 @@ def response_message(event):
                                        text=f"User Id: {profile.user_id[:5]}...\n"
                                             f"Status Message: {status_msg}",
                                        actions=[MessageAction(label="成功", text="次は何を実装しましょうか？")]))
-    
+
     line_bot_api.reply_message(event.reply_token, messages=messages)
-    
-#    # 退出処理
-#    if event.message.text == "帰って":
-#        line_bot_api.reply_message(event.reply_token, TextSendMessage("うっうっ"))
-#
-#        #グループトークからの退出処理
-#        if hasattr(event.source,"group_id"):
-##           line_bot_api.leave_group(event.source.group_id)
-#
-#        #ルームからの退出処理
-#        if hasattr(event.source,"room_id"):
-#            line_bot_api.leave_room(event.source.room_id)
-#
-#        return
-#    
-#    if event.message.text == "ごめんね":
-#        line_bot_api.reply_message(event.reply_token, TextSendMessage("いいよ"))
-#
-#    # オウム返し
-#    line_bot_api.reply_message(
-#        event.reply_token,
-#        TextSendMessage(text=event.message.text))
-    
-    
+
+
 if __name__ == "__main__":
-#    app.run()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
